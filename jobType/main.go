ef30e207ada7cff55473abc6062cf0091df9aa65
elijah <cuiyeqing960904@gmail.com>
2025-08-27 23:28:40 +0300

    wait group + basic

---
+++ jobType/main.go
@@ -0,0 +1,1 @@
+package jobType
--- waitGroup/main.go
+++ waitGroup/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func worker(id int, jobs <-chan int, results chan<- int) {
+func worker(id int, jobs <-chan int, results chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for j := range jobs {
-		fmt.Printf("worker %d received job %d\n", id, j)
+		fmt.Printf("Worker %d started job %d\n", id, j)
 		time.Sleep(time.Second)
 		fmt.Printf("Worker %d finished job %d\n", id, j)
 		results <- j * 2
@@ -18,17 +20,21 @@ func main() {
 	const numJobs = 5
 	jobs := make(chan int, numJobs)
 	results := make(chan int, numJobs)
+	var wg sync.WaitGroup
 
+	wg.Add(3)
 	for w := 1; w <= 3; w++ {
-		go worker(w, jobs, results)
+		go worker(w, jobs, results, &wg)
 	}
 
 	for j := 1; j <= numJobs; j++ {
 		jobs <- j
 	}
 	close(jobs)
+	wg.Wait()
+	close(results)
 
-	for a := 1; a <= numJobs; a++ {
-		fmt.Println(<-results)
+	for result := range results {
+		fmt.Println(result)
 	}
 }
