f38feeb4b63739ed1e2387e33a185c3b1624b1cd
elijah <cuiyeqing960904@gmail.com>
2025-09-15 23:52:27 +0300

    pipeline pattern

--- pipeline/main.go
+++ pipeline/main.go
@@ -1,3 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+func generator(input []int) chan int {
+	ch := make(chan int)
+	go func() {
+		defer close(ch)
+		for _, data := range input {
+			ch <- data
+		}
+	}()
+	return ch
+}
+
+func add(inputCh chan int) chan int {
+	res := make(chan int)
+	go func() {
+		defer close(res)
+		for data := range inputCh {
+			res <- data + 1
+		}
+	}()
+
+	return res
+}
+
+func multiply(inputCh chan int) chan int {
+	res := make(chan int)
+    go func() {
+        defer close(res)
+        for data := range inputCh {
+            res <- data * 2
+        }
+    }()
+    return res
+}
+
 func main() {
-	
+	input := []int{1, 2, 3}
+    inputCh := generator(input)
+    resultCh := multiply(add(inputCh))
+    for res := range resultCh {
+        fmt.Println(res)
+    }
 }
