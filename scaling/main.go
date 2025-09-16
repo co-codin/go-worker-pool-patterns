02536b4abf38aff429cb1aa01277fa5aab5cd75a
elijah <cuiyeqing960904@gmail.com>
2025-09-16 12:07:06 +0300

    testing

--- scaling/main.go
+++ scaling/main.go
@@ -1,15 +1,18 @@
 package main
 
-import "sync"
-
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 type DynamicPool struct {
 	minWorkers int
 	maxWorkers int
-	jobQueue chan int
-	wg sync.WaitGroup
-	mu sync.Mutex
-	active int
+	jobQueue   chan int
+	wg         sync.WaitGroup
+	mu         sync.Mutex
+	active     int
 }
 
 func NewDynamicPool(min, max int) *DynamicPool {
@@ -21,5 +24,43 @@ func NewDynamicPool(min, max int) *DynamicPool {
 }
 
 func (dp *DynamicPool) startWorker() {
-	
+	dp.mu.Lock()
+	dp.active++
+	dp.mu.Unlock()
+	dp.wg.Add(1)
+	go func() {
+		defer dp.wg.Done()
+		defer func() {
+			dp.mu.Lock()
+			dp.active--
+			dp.mu.Unlock()
+		}()
+		for job := range dp.jobQueue {
+			fmt.Printf("Processing job %d\n", job)
+			time.Sleep(time.Second)
+		}
+	}()
+}
+
+func (dp *DynamicPool) AddJob(job int) {
+	dp.jobQueue <- job
+	dp.mu.Lock()
+	if len(dp.jobQueue) > dp.active && dp.active < dp.maxWorkers {
+		dp.startWorker()
+	}
+	dp.mu.Unlock()
+}
+
+
+func (dp *DynamicPool) Shutdown() {
+	close(dp.jobQueue)
+	dp.wg.Wait()
+}
+
+func main() {
+	dp := NewDynamicPool(2, 5)
+	for i := 1; i <= 10; i++ {
+		dp.AddJob(1)
+	}
+	dp.Shutdown()
 }
