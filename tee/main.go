f0621a28b05f6359a833183ecb28e7a86546fd5b
elijah <cuiyeqing960904@gmail.com>
2025-09-17 20:28:55 +0300

    more dynamic

--- tee/main.go
+++ tee/main.go
@@ -3,42 +3,39 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
-func tee(in <- chan int,) (_, _ <- chan int) {
-	out1 := make(chan int)
-	out2 := make(chan int)
+func tee(in <-chan int, numChans int) []chan int {
+	chans := make([]chan int, numChans)
+
+	for i := range numChans {
+		chans[i] = make(chan int)
+	}
 
 	go func() {
-		defer close(out1)
-		defer close(out2)
+		for i := range numChans {
+			defer close(chans[i])
+		}
 
 		for val := range in {
 			wg := &sync.WaitGroup{}
 
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				out1 <- val
-			}()
-
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				out2 <- val
-			}()
-			
+			for i := range numChans {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					chans[i] <- val
+				}()
+			}
 			wg.Wait()
 		}
 	}()
 
-	return out1, out2
+	return chans
 }
 
 func generate() chan int {
 	ch := make(chan int)
-
 
 	go func() {
 		for i := range 5 {
@@ -51,23 +48,23 @@ func generate() chan int {
 }
 
 func main() {
-	ch1, ch2 := tee(generate())
+	chans := tee(generate(), 2)
 
-	wg := sync.WaitGroup{}
+	wg := &sync.WaitGroup{}
 	wg.Add(2)
 	go func() {
 		defer wg.Done()
-		for v := range ch1 {
+		for v := range chans[0] {
 			fmt.Println("logging...", v)
 		}
 	}()
 
 	go func() {
 		defer wg.Done()
-		for v := range ch2 {
-			time.Sleep(time.Second)
-			fmt.Println("writing...", v)
+		for v := range chans[1] {
+			fmt.Println("logging...", v)
 		}
 	}()
+
 	wg.Wait()
 }
