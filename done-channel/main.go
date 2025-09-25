8da09e320037c39c18cee7774d17491e2f3c4200
elijah <cuiyeqing960904@gmail.com>
2025-09-25 12:14:32 +0300

    done channel

--- done-channel/main.go
+++ done-channel/main.go
@@ -1,3 +1,31 @@
 package main
 
+import "fmt"
 
+func process(closeCh <-chan struct{}) <-chan struct{} {
+	closeDoneCh := make(chan struct{})
+
+	go func() {
+		defer close(closeDoneCh)
+		for {
+			select {
+			case <- closeCh:
+				return
+			default:
+				// processing
+			}
+		}
+	}()
+
+	return closeDoneCh
+}
+
+func main() {
+	closeCh := make(chan struct{})
+	closeDoneCh := process(closeCh)
+
+	close(closeCh)
+	<-closeDoneCh
+
+	fmt.Println("done")
+}
